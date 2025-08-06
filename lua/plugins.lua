6a14ae94c9624e6881bb6cdb823cf23f51502fa9
mushroomate <127420250+mushroomate@users.noreply.github.com.>
2025-08-06 11:53:32 +0900

    feat: add Neogit plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -58,6 +58,20 @@ require("lazy").setup({
             })
         end,
     },
+
+    {
+        "NeogitOrg/neogit",
+        dependencies = {
+            "nvim-lua/plenary.nvim",         -- required
+            "sindrets/diffview.nvim",        -- optional - Diff integration
+
+            -- Only one of these is needed.
+            "nvim-telescope/telescope.nvim", -- optional
+            "ibhagwan/fzf-lua",              -- optional
+            "echasnovski/mini.pick",         -- optional
+            "folke/snacks.nvim",             -- optional
+        },
+    },
     -- Vscode-like pictograms
     {
         "onsails/lspkind.nvim",
