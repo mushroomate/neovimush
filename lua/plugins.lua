a68ec3d3f146648f4bb6fc181d33853beb40c4b4
mushroomate <127420250+mushroomate@users.noreply.github.com.>
2025-12-15 16:00:41 +0900

    fix: update sonet model setting to lastest for avante

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -240,10 +240,11 @@ require("lazy").setup({
             providers = {
                 claude = {
                     endpoint = "https://api.anthropic.com",
-                    model = "claude-3-5-sonnet-20241022",
+                    model = "claude-sonnet-4-20250514",
+                    timeout = 30000,
                     extra_request_body = {
                         temperature = 0.75,
-                        max_tokens = 4096,
+                        max_tokens = 20480,
                     },
                 },
                 deepseek = {
