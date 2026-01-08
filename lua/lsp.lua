5369aeeee14e6a91597b179ba1f28329efd80db5
mushroomate <127420250+mushroomate@users.noreply.github.com.>
2026-01-08 16:49:06 +0900

    fix: readd biome LSP, and enable it with last format

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -10,12 +10,12 @@ require('mason').setup({
 
 --- todo: add Linter & Formatter modules
 ---       including: 'prettier'
-local ensure_installed_lsp = { 'pylsp', 'ruff', 'lua_ls', 'rust_analyzer', 'marksman', 'omnisharp', 'nil_ls' }
+local ensure_installed_lsp = { 'pylsp', 'ruff', 'lua_ls', 'rust_analyzer', 'marksman', 'omnisharp', 'nil_ls', 'biome' }
 
 -- Discard LSPs which do not supported by Windows
 if vim.fn.has("win32") == 1 then
     local windows_exclude = { 'nil_ls' }
-    
+
     for i = #ensure_installed_lsp, 1, -1 do
         local current_tool = ensure_installed_lsp[i]
 
@@ -50,7 +50,7 @@ vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
+local on_attach = function(bufnr)
     -- Enable completion triggered by <c-x><c-o>
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
@@ -82,26 +82,26 @@ local lsp_capabilities = cmp_nvim_lsp.default_capabilities()
 cmp.setup({
     snippet = { ... },
     window = {
-      completion = cmp.config.window.bordered(),
-      documentation = cmp.config.window.bordered(),
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
     },
     mapping = cmp.mapping.preset.insert({
-      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
     sources = cmp.config.sources({
-      { name = 'lazydev' },
-      { name = 'nvim_lsp' },
-      -- { name = 'vsnip' }, -- For vsnip users.
-      { name = 'luasnip' }, -- For luasnip users.
-      -- { name = 'ultisnips' }, -- For ultisnips users.
-      -- { name = 'snippy' }, -- For snippy users.
+        { name = 'lazydev' },
+        { name = 'nvim_lsp' },
+        -- { name = 'vsnip' }, -- For vsnip users.
+        { name = 'luasnip' }, -- For luasnip users.
+        -- { name = 'ultisnips' }, -- For ultisnips users.
+        -- { name = 'snippy' }, -- For snippy users.
     }, {
-      { name = 'buffer' },
-      { name = 'path'},
+        { name = 'buffer' },
+        { name = 'path' },
     })
 })
 
@@ -111,16 +111,19 @@ local common_config = {
     capabilities = lsp_capabilities,
 }
 
--- 配置每个语言服务器
-vim.lsp.config['pylsp'] = vim.tbl_deep_extend('force', common_config, {
-    -- 可以添加 pylsp 特有的设置
-})
 
-vim.lsp.config['ruff'] = vim.tbl_deep_extend('force', common_config, {
-    -- ruff 特有设置
-})
+local util = require("lspconfig.util")
+local lspconfig = require("lspconfig")
+local servers = {}
 
-vim.lsp.config['lua_ls'] = vim.tbl_deep_extend('force', common_config, {
+servers.pylsp = {}
+servers.ruff = {}
+servers.rust_analyzer = {}
+servers.marksman = {}
+servers.omnisharp = {}
+servers.nil_ls = {}
+
+servers.lua_ls = {
     settings = {
         Lua = {
             runtime = { version = 'LuaJIT' },
@@ -129,23 +132,32 @@ vim.lsp.config['lua_ls'] = vim.tbl_deep_extend('force', common_config, {
             telemetry = { enable = false },
         }
     }
-})
+}
 
-vim.lsp.config['rust_analyzer'] = vim.tbl_deep_extend('force', common_config, {
-    -- rust_analyzer 特有设置
-})
+servers.biome = {
+    filetypes = {
+        "javascript", "javascriptreact", "typescript", "typescript.tsx",
+        "json", "jsonc", "css", "svelte", "vue", "astro"
+    },
+    single_file_support = true,
+    root_dir = function(fname)
+        -- fname 是文件路径(string)
+        local root_file = util.root_pattern("biome.json", "biome.jsonc")(fname)
+        if root_file then
+            return root_file
+        end
 
-vim.lsp.config['marksman'] = vim.tbl_deep_extend('force', common_config, {
-    -- marksman 特有设置
-})
+        local git_root = util.root_pattern(".git")(fname)
+        if git_root then
+            return git_root
+        end
 
-vim.lsp.config['omnisharp'] = vim.tbl_deep_extend('force', common_config, {
-})
+        return util.path.dirname(fname)
+    end,
+}
 
-vim.lsp.config['nil_ls'] = vim.tbl_deep_extend('force', common_config, {
-})
 
--- 启用所有已配置的服务器
-for server_name, _ in pairs(vim.lsp.config) do
-    vim.lsp.enable(server_name)
+for name, config in pairs(servers) do
+    local final_config = vim.tbl_deep_extend("force", common_config, config)
+    lspconfig[name].setup(final_config)
 end
